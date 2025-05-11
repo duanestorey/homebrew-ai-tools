40393b8cad0d3c84062fcedaf17d9b247844c225
GitHub Action <action@github.com>
2025-05-11 14:11:49 +0000

    Update to version 1.0.6

--- ai-tools.rb
+++ ai-tools.rb
@@ -1,8 +1,8 @@
 class AiTools < Formula
   desc "Generate AI-friendly project overviews"
   homepage "https://github.com/duanestorey/ai-tools"
-  url "https://github.com/duanestorey/ai-tools/archive/refs/tags/1.0.5.tar.gz"
-  sha256 "9dacd91aca044bc160df08e7d5a3adf8588254d5f5491301bfc806988be19600"
+  url "https://github.com/duanestorey/ai-tools/archive/refs/tags/1.0.6.tar.gz"
+  sha256 "3eec717ae6221b46efd111a99fcf5262469eb0bf81f2dda43ea431214aeef978"
   license "MIT"
   
   depends_on "php"
